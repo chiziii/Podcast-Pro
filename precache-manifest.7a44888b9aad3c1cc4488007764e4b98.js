b56d8d4d6b943b526d495bad5b9065470a226c87
Chiziii <igwechizy@gmail.com>
2020-06-09 23:37:26 +0100

    Updates

---
+++ precache-manifest.7a44888b9aad3c1cc4488007764e4b98.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2100f92bc6aa0d8e141dc50f7d98a1eb",
+    "url": "/Podcast-Pro/index.html"
+  },
+  {
+    "revision": "377fa1e76bd841a1d35e",
+    "url": "/Podcast-Pro/static/css/main.aefc2812.chunk.css"
+  },
+  {
+    "revision": "c1b10e72b2577a0bfba5",
+    "url": "/Podcast-Pro/static/js/2.b82930fa.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/Podcast-Pro/static/js/2.b82930fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "377fa1e76bd841a1d35e",
+    "url": "/Podcast-Pro/static/js/main.496e6296.chunk.js"
+  },
+  {
+    "revision": "992e2b4fd20e7a50c7d6",
+    "url": "/Podcast-Pro/static/js/runtime-main.0fb4d855.js"
+  },
+  {
+    "revision": "1b580d980532792578c54897ca387e2c",
+    "url": "/Podcast-Pro/static/media/Helvetica.1b580d98.ttf"
+  },
+  {
+    "revision": "a85aa8bc25f5a1ddac56f45a46849ff2",
+    "url": "/Podcast-Pro/static/media/RecOne.a85aa8bc.png"
+  },
+  {
+    "revision": "d0285caff576f8650267ff36e41e6908",
+    "url": "/Podcast-Pro/static/media/RecTwo.d0285caf.png"
+  }
+]);
